004d5b2ca01c022080ffb7d6df821f8f5b917bcd
MauriceElliott <mauricemelliott@gmail.com>
2025-02-24 23:47:04 +0000

    restructuring folders

---
+++ src/monkey/lexer/lexer_test.go
@@ -0,0 +1,32 @@
+package lexer
+
+import (
+	"money/token"
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+	}
+}
