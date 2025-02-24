4f554ff5ee5373b52de7a744d6c846cc5909e00d
MauriceElliott <mauricemelliott@gmail.com>
2025-02-24 23:59:17 +0000

    going to bed

--- src/monkey/lexer/lexer_test.go
+++ src/monkey/lexer/lexer_test.go
@@ -1,7 +1,7 @@
 package lexer
 
 import (
-	"money/token"
+	"monkey/token"
 	"testing"
 )
 
@@ -27,6 +27,12 @@ func TestNextToken(t *testing.T) {
 
 	for i, tt := range tests {
 		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
 
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
 	}
 }
